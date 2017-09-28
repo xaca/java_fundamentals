7ef731741c15dea58b4f0c210e4f56125f7273d1
Nahuel <nahueldolores@hotmail.com>
2017-09-27 23:29:12 -0300

    Condicionales Dificil

---
+++ 03 condicionales/CondicionalesDificil.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+/* Ejercicios con condicionales, NIVEL: DIFICIL */
+public class CondicionalesDificil {
+
+	public static void main(String[] args) {
+		int examnPrimerTermino;
+    int examenMedioTermino;
+    int examenUltimoTermino;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Introduce el resultado de el examen de primer termino: ");
+		examnPrimerTermino = scanner.nextInt();
+    
+		System.out.println("Introduce el resultado de el examen de medio termino: ");
+		examenMedioTermino = scanner.nextInt();
+    
+		System.out.println("Introduce el resultado de el examen de ultimo termino: ");
+		examenUltimoTermino = scanner.nextInt();
+    
+		/**
+		* Escribe una solucion logica con condicionales que:
+    * Se introduce la puntuacion de 3 diferentes terminos. Debes obtener un promedio e imprimir:
+    * "Puntuación A" si el promedio es mayor o igual al 90%
+    * "Puntuación B" si el promedio se encuentra entre el 70% y 90%
+    * "Puntuación C" si el promedio se encuentra entre el 50% y 70%
+    * "Puntuación F" si el promedio es menor que el 50%
+    * y "Invalido" si el resultado es invalido.
+		*/
+		
+	}
+
+}
