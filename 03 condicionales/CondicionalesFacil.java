10bf0c1a2dcd43ae1df1dc498254cd2af8b598cd
Nahuel <nahueldolores@hotmail.com>
2017-09-27 22:34:34 -0300

    First conditionals exercise
    Check 'em

---
+++ 03 condicionales/CondicionalesFacil.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+/* Ejercicios con condicionales, NIVEL: FACIL */
+public class CondicionalesFacil {
+
+	public static void main(String[] args) {
+		int edad;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Cual es tu edad?")
+		edad = scanner.nextInt();
+		
+		/**
+		* Escribe una solucion logica con condicionales:
+		* Para que una persona pueda votar, esta debe tener una edad mayor (o igual) a 18 años
+		* Al finalizar el ejercicio, prueba tu programa introduciendo un numero
+		*/
+		
+	}
+
+}
