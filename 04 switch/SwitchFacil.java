6b6bc69c15d9b784c61cd69bad8449cef7f1888b
Nahuel <nahueldolores@hotmail.com>
2017-09-28 21:43:07 -0300

    Easy switch exersice
    We goin' in!

---
+++ 04 switch/SwitchFacil.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+/* Ejercicios con operador "switch", NIVEL: FACIL */
+public class SwitchFacil {
+
+	public static void main(String[] args) {
+		int number;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Presiona un numero!");
+		number = scanner.nextInt();
+
+		/**
+		* Escribe una solucion con el operador "switch" en el que:
+		* El usuario introduce un numero (de 0 a 9), imprime el numero presionado,
+		* en el caso de un numero que no se encuentre, imprimir "Invalido".
+		*/
+		
+	}
+
+}
