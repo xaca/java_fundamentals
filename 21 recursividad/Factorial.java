832619a6d10f532ca5eaf044c7e2e0dc2bb5c44e
agampreetsingh <agampreet_singh@yahoo.com>
2017-09-29 18:20:21 +0530

    Update Factorial.java
    Optimized the code for larger values.

--- 21 recursividad/Factorial.java
+++ 21 recursividad/Factorial.java
@@ -1,10 +1,11 @@
 public class Factorial {
-    public static int factorial(int num) {
-        if (num > 1) {
-            return num * factorial(num - 1);
-        }
-        
-        return num;
+    public static BigInteger factorial(int n)
+    {
+        BigInteger p=new BigInteger("1");
+        if(n==1 || n==0)
+            return BigInteger.valueOf(1);
+        p=BigInteger.valueOf(n).multiply(fact(n-1));
+        return p;
     }
 
     public static void main(String[] args) {
