61e5f9ed6d84aa4041ae0ec91fb074c61e3c57c0
Jorge Londoño <jmlon@yahoo.com>
2017-09-29 18:32:28 -0500

    Created a recursive example for converting from decimal to base B

---
+++ 21 recursividad/Dec2BaseP.java
@@ -0,0 +1,34 @@
+public class Dec2BaseP {
+
+
+    public static void dec2BaseP(int decimal, int p) {
+        if (decimal>0) {
+            int residue = decimal % p;
+            decimal /= p;
+            dec2BaseP(decimal,p);
+            System.out.print(digit2char(residue));
+        }
+    }
+    
+    private static char digit2char(int digit) {
+        if (digit<=9) return (char)('0'+digit);
+        else return (char)('A'+(char)digit);
+    }
+
+
+    public static void main(String[] args) {
+        dec2BaseP(257,2); // Binary
+        System.out.println();
+    
+        dec2BaseP(257,8); // Octal
+        System.out.println();
+
+        dec2BaseP(257,16); // Hexadecimal
+        System.out.println();
+
+    }
+
+
+}
+
+
