9466239f0840c66c08aa9fabadeb55373995e6c5
nguyendhse <nguyendhse@gmail.com>
2017-10-03 00:55:10 +0700

    update sqrt funtion name

--- 21 recursividad/sqrt.java
+++ 21 recursividad/sqrt.java
@@ -1,13 +1,13 @@
 public class Sqrt {
     public static void main(String[] args) {
-        System.out.println(sqrt3(8.0, 3)); // result: 2.0
+        System.out.println(sqrtn(8.0, 3)); // result: 2.0
     }
 
     /*
     *   nth root of number x
     *
     */
-    public static double sqrt3(double x, int nth) {
+    public static double sqrtn(double x, int nth) {
         if (x == 0)
             return 0;
         if (x < 0)
