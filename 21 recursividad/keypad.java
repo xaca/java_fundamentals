a18441ead0145f5c014d5c9ea740faa36d8d9653
Kartik Kwatra <kartikkwatra2016@gmail.com>
2017-10-03 20:57:16 +0530

    Comments and indentation

--- 21 recursividad/keypad.java
+++ 21 recursividad/keypad.java
@@ -1,29 +1,59 @@
 public class solution {
 
-	// Return a string array that contains all the possible strings
+	/*
+    Given an integer n, using alpha-numeric keypad return all strings that can be made using digits of n
+
+    Sample input:
+    23
+
+    Sample output:
+    ad
+    ae
+    af
+    bd
+    be
+    bf
+    cd
+    ce
+    cf
+    */ 
+
+
+    //helper function
 	public static String[] keypad(int n){
-	String[] map={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-	String[] newans=alphaNum(n,map);	// Write your code here
+	String[] map={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};//map of alphanumeric keys
+	String[] newans=alphaNum(n,map);
     return newans;
 	}
+
+    //recursive function
 	public static String[] alphaNum(int num,String[] map) {
 		
+        //base case
 		if(num==0){
 			String[] ans={""};
 			return ans;
 		}
-		String smallAns[]=alphaNum(num/10, map);
+
+        
+		String smallAns[]=alphaNum(num/10, map);//recursive call
+
 		int lastdigit=num%10;
 		String lastdigitopt=map[lastdigit];
+
+        //variable to store answer of current function call
 		String ans[]=new String[smallAns.length*lastdigitopt.length()];
 		int k=0;
-		
+
+		//concatinate words from smallans with lastDigitOptions and store in answer
 		for(int i=0;i<lastdigitopt.length();i++){
 			for(int j=0;j<smallAns.length;j++){
 				ans[k]=smallAns[j] + lastdigitopt.charAt(i);
 				k++;
 			}
 		}
+
+        //return answer
 		return ans;
 	}
 }
