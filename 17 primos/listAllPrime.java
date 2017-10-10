7b98c20320533b3791c145a2467b9fcaf2d57788
Ganesh varma <f2015284@hyderabad.bits-pilani.ac.in>
2017-10-10 19:39:25 +0530

    used static method in ListAllPrime class

--- 17 primos/listAllPrime.java
+++ 17 primos/listAllPrime.java
@@ -13,10 +13,10 @@
  23
  29
  */
-class Prime{
-  int i;
-  Prime(int a){
-    for(i=2;i<=a;i++){
+class ListAllPrime{
+ 
+  static void prime(int a){
+    for(int i=2;i<=a;i++){
       int counter=0;
       for(int b=1;b<=i;b++){
         if(i%b==0){
@@ -28,12 +28,12 @@ class Prime{
       }
     }
   }
-
-}
-class ListAllPrime{
+ 
   public static void main(String args[]){
     int m;
     m=Integer.parseInt(args[0]);
-    Prime p=new Prime(m);
+    ListAllPrime.prime(m);
+
+
   }
 }
