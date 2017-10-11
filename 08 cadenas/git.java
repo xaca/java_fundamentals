2987fb36f2fa165df978804cbedd9f178abea614
Joseph Walton-Rivers <jwalto@essex.ac.uk>
2017-10-11 10:14:50 +0100

    fix the strange code formatting in the 'git' task

--- 08 cadenas/git.java
+++ 08 cadenas/git.java
@@ -31,21 +31,22 @@ public class Git {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-     int qtd, y=0, pok=0;
-       String s;
-       Scanner ler = new Scanner(System.in);
-       Vector<String> v = new Vector();
-       qtd = ler.nextInt();
-        while(y<qtd){
+        int qtd, y=0, pok=0;
+        String s;
+        Scanner ler = new Scanner(System.in);
+
+        Vector<String> v = new Vector();
+        qtd = ler.nextInt();
+
+        while(y<qtd) {
             y++;
             s = ler.next();
             if (!v.contains(s) && v.size() < 152) v.add(s);
         }
-         System.out.print("Missing ");
-         System.out.print(151-v.size());
-         System.out.print(" Pokemons\n");
 
+        System.out.print("Missing ");
+        System.out.print(151-v.size());
+        System.out.print(" Pokemons\n");
     }
     
 }
