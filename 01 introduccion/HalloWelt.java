60dff176ed0ca75600670dd5226232360dc65e3f
licsth <32680008+licsth@users.noreply.github.com>
2017-10-17 15:49:26 +0200

    Update HalloWelt.java

--- 01 introduccion/HalloWelt.java
+++ 01 introduccion/HalloWelt.java
@@ -1,7 +1,7 @@
-public class HolaMundo{
+public class HalloWelt{
 	public static void main(String args[])
 	{
-    //German / Deutsch
+    		//German / Deutsch
 		System.out.println("Hallo Welt");
 	}
 }
