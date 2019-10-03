655344ce605e4fe70a26d51d0041f15f5d11637d
Niklas Schultz <thexsalkin@gmail.com>
2019-10-03 10:04:25 +0200

    The "fibonacci" module is now runnable as a jar
    I've added maven-jar-plugin so when building this module
    via "mvn clean install" we can actually run the outputed jar
    file. Before this change we got "no main manifest attribute" error.
    
    I also had to slightly adjust the Fibonacci.java file so it contains
    a proper main method.

--- 16 fibonacci/src/main/java/Fibonacci.java
+++ 16 fibonacci/src/main/java/Fibonacci.java
@@ -1,5 +1,10 @@
 public class Fibonacci {
 
+	public static void main(String [] args) {
+		String result = calcularNumeros(Integer.valueOf(args[0]));
+		System.out.println(result);
+	}
+
     public static String calcularNumeros(int n) {
         StringBuilder fibonacciString = new StringBuilder();
 
